ab9ff7452069fb959a5144a210212c6fb35a5cad
Nathan Coupermant <nathan.coupermant@epita.fr>
2019-02-28 17:37:55 +0100

    head start file

---
+++ 2019/main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <cstdlib>
+#include <limits>
+#include <cstdint>
+#include <string>
+#include <fstream>
+
+
+int main()
+{
+    return 0;
+}
