3003507291c84b76403123a4755a6fb5361cac09
Martin Huvelle <mhuvelle@lrde.epita.fr>
2019-02-28 20:29:36 +0800

    submit script

---
+++ 2019/submit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cat test/a_example.txt | ./main > a
+cat test/b_example.txt | ./main > b
+cat test/c_example.txt | ./main > c
+cat test/d_example.txt | ./main > d
+cat test/e_example.txt | ./main > e
