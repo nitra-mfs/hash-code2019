132734472b2eb1e369ac087afd1217c097a30a15
Nathan Coupermant <nathan.coupermant@epita.fr>
2019-02-28 19:18:49 +0100

    outter

--- 2019/main.cc
+++ 2019/main.cc
@@ -8,6 +8,17 @@
 #include <string>
 #include <fstream>
 
+#include "slide.hh"
+#include "parser.hh"
+
+template<class Out>
+Out& operator<<(Out& out, const Slide& s)
+{
+    out << s.pics_.size();
+    for (const auto& v : s.pics_)
+        out << v.tags;
+    return out;
+}
 
 int main()
 {
